465b19005ac62cbc0da4e9ed9f79cc8591554781
pchandt <pcthakuri26@gmail.com>
2022-10-12 14:14:00 -0700

    end game method created, AB#14

--- src/main/java/com/teamguppy/Main.java
+++ src/main/java/com/teamguppy/Main.java
@@ -1,61 +1,19 @@
 package com.teamguppy;
 
 import com.teamguppy.controller.Controller;
+import com.teamguppy.model.Game;
 import com.teamguppy.view.View;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
+
     View view = new View();
     Controller controller = new Controller(view);
     controller.displayAsciiBanner();
     controller.displayAboutGame();
 
-    String location = "Ocean floor";
-    try (Scanner sc = new Scanner(System.in)) {
-      label:
-      do {
-        if (location.equals("Ocean floor")) {
-          System.out.println("Welcome! enter yes to continue and quit to end");
-          String command = sc.nextLine();
-          switch (command) {
-            case "yes":
-              startGame();
-              break label;
-            case "quit":
-              endGame();
-              break label;
-
-          }
-        }
-
-      } while (true);
-    }
-
+    Game.landingRoom();
   }
 
-  private static void startGame() {
-    System.out.println("Enter go south to enter into new room");
-    try(Scanner sc =  new Scanner(System.in)) {
-      do {
-        String command  =  sc.nextLine();
-        switch (command) {
-          case "go south":
-            System.out.println("You are in ocean forest");
-            break;
-          case "go east":
-            System.out.println("You are in sunk ship");
-        }
-      } while (true);
-    }
-
-
-  }
-
-  private static void endGame() {
-    System.out.println("Bye! See you later");
-
-  }
 }
