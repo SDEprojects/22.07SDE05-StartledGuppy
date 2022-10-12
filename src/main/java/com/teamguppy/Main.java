33fce77a7127038f6a25ccbe7c13b88c364cbb0c
Quentin M <mattson.quentin@gmail.com>
2022-10-12 12:26:53 -0700

    Title ASCII Art added, AB#11

--- src/main/java/com/teamguppy/Main.java
+++ src/main/java/com/teamguppy/Main.java
@@ -1,5 +1,14 @@
 package com.teamguppy;
 
+import com.teamguppy.controller.Controller;
+import com.teamguppy.view.View;
+
 public class Main {
 
+  public static void main(String[] args) {
+    View view = new View();
+    Controller controller = new Controller(view);
+    controller.displayAsciiBanner();
+
+  }
 }
