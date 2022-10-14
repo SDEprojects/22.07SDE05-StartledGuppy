7cc2ababe9df178a7785afa7ee0eb114b1d8438f
pchandt <pcthakuri26@gmail.com>
2022-10-13 21:25:59 -0700

    method to display information about room is created, AB#19

--- src/main/java/com/teamguppy/model/Game.java
+++ src/main/java/com/teamguppy/model/Game.java
@@ -24,8 +24,6 @@ public class Game {
 
   public static void landingRoom() throws IOException, ParseException {
     String command = null;
-
-
     try (Scanner sc = new Scanner(System.in)) {
       label:
       do {
@@ -41,37 +39,23 @@ public class Game {
               break label;
           }
         }
-      } while(validStartInput(command));
+      } while (validStartInput(command));
 
     }
   }
 
 
-  private static Boolean validStartInput(String input){
-    if (!input.equals("yes") || !input.equals("quit")){
-      System.out.println("Sorry, I don't understand" );
-    }return(true);
+  private static Boolean validStartInput(String input) {
+    if (!input.equals("yes") || !input.equals("quit")) {
+      System.out.println("Sorry, I don't understand");
+    }
+    return (true);
   }
+
   private static void startGame() throws IOException, ParseException {
-
-
-    userMove();
-    System.out.println("Enter 'go south' or 'go east' or 'go west' to enter into new room.");
-    try (Scanner sc = new Scanner(System.in)) {
-      do {
-        String command = sc.nextLine();
-        switch (command) {
-          case "go south":
-            System.out.println("You are in ocean forest.");
-            break;
-          case "go east":
-            System.out.println("You are in sunk ship.");
-            break;
-          case "go west":
-            System.out.println("You have encountered a goblin shark.");
-        }
-      } while (true);
-    }
+    do {
+      userMove();
+    } while (true);
   }
 
 
@@ -83,16 +67,14 @@ public class Game {
     String input = sc.nextLine();
     if (input.equals("yes")) {
       System.out.println("Bye! See you later.");
-
     } else if (input.equals("no")) {
       landingRoom();
     }
   }
 
-
   // parsing user input for the verb + noun
   // we can make function for each verb, and call the function in here
-  
+
   private static void userMove() throws IOException, ParseException {
     boolean valid;
     String move;
@@ -105,26 +87,29 @@ public class Game {
       move = arr[0];
       item = arr[1];
       valid = validMove(move);
-      System.out.println(valid);
-    }while(!valid);
+//      System.out.println(valid);
+    } while (!valid);
     if (move.equals("go")) {
-     currentLocation = Location.findLocation(currentLocation.toString(), item);
       //function for go
+      currentLocation = Location.findLocation(currentLocation.toString(), item);
       System.out.println("Your current location " + currentLocation);
+      Location.roomDescription(currentLocation.toString());
     } else if (move.equals("look")) {
       System.out.println("not working");
       //function for look
     }
+
   }
 
   // validate user's move input
-  private static Boolean validMove(String move){
+  private static Boolean validMove(String move) {
     System.out.println("this " + move);
     if (move.equals("go") || move.equals("look") || move.equals("help") || move.equals("ask")) {
       return true;
-    }else {
+    } else {
       System.out.println("Sorry, I don't understand. Please check the Game Commands.");
-    } return false;
+    }
+    return false;
   }
 }
 
