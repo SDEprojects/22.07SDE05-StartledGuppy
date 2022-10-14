3ea163332d6f5939700d82c67df9a55c5b34fbb8
alice <aliceahuh213@gmail.com>
2022-10-14 14:15:31 -0400

    add item data in json file

---
+++ src/main/java/com/teamguppy/model/Item.java
@@ -0,0 +1,26 @@
+package com.teamguppy.model;
+
+public class Item {
+
+  private static String name;
+  private static String description;
+
+  public Item() {
+    this.name = name;
+    this.description = description;
+  }
+
+  public static String getName() {
+    return name;
+  }
+
+  public static String getDescription() {
+    return description;
+  }
+
+  public static String showDescription(String item){
+
+
+  }
+
+}
